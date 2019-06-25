6b42f062e257ed392e4150034dc1e062751aeb36
effiong-jr <f.young.de@gmail.com>
2019-06-25 01:17:20 +0100

    Updates

---
+++ precache-manifest.d1f8612e36ad8a52db46520c2ad80798.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc496585a66a5af2830246fae5c5b57a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "001139e34ba8c3f6abed",
+    "url": "/portfolio/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "7b772ed3355fdd5c2338",
+    "url": "/portfolio/static/css/main.a79e4762.chunk.css"
+  },
+  {
+    "revision": "001139e34ba8c3f6abed",
+    "url": "/portfolio/static/js/2.987e4ee7.chunk.js"
+  },
+  {
+    "revision": "7b772ed3355fdd5c2338",
+    "url": "/portfolio/static/js/main.330e8dae.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "2be076effa2baa28598c1d4eed8c752f",
+    "url": "/portfolio/static/media/me.2be076ef.jpg"
+  },
+  {
+    "revision": "1d708b2c3e4bdf513e0fa8290cb6814c",
+    "url": "/portfolio/static/media/pixelArtMaker.1d708b2c.PNG"
+  },
+  {
+    "revision": "2b59a97c3d33f2ee1732d2d0ab4213f7",
+    "url": "/portfolio/static/media/quoteMachine.2b59a97c.PNG"
+  },
+  {
+    "revision": "5bc6d7d5d7a5d60962ec3e16c66a862a",
+    "url": "/portfolio/static/media/technicalDoc.5bc6d7d5.PNG"
+  }
+]);
