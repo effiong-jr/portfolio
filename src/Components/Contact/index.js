4aa583903d6855232efee93d1d03f67157cbb378
effiong-jr <f.young.de@gmail.com>
2019-06-27 13:27:32 +0100

    updated contact page

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -5,12 +5,12 @@ function Contact() {
     return (
         <div id="contact" className="container">
             <h1 className="heading">Contact:</h1>
-            
+            <p>Get it touch with me via:</p>
             <ul>
-                <li>Email: <a href="mailto:f.young.de@gmail.com">f.young.de@gmail.com</a></li>
                 <li>Twitter: <a href="https://twitter.com/Fyoung_Jr">@Fyoung_Jr</a></li>
+                <li>Email: <a href="mailto:f.young.de@gmail.com">f.young.de@gmail.com</a></li>
             </ul>
-            <p className="text-center">Let's build some cool stuffs together...</p>
+            <p className="text-center">...and let's build some cool stuffs together!</p>
 
         </div>
     )
