ba10eb5d170055c34e81a6e492354b3dd7830973
effiong-jr <f.young.de@gmail.com>
2019-07-20 14:44:56 +0100

    Changed footer position to absolute

--- src/App.js
+++ src/App.js
@@ -14,15 +14,17 @@ function App() {
     <div className="container col-md-6 col-xs-12">
       <Router>
         <Navbar />
-          
-        <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/about' component ={About} />
-          <Route path='/projects' component ={Projects} />
-          <Route path='/contact' component={Contact} />
-          <Redirect to="/" />
+        <div className="content-wrapper">
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/about' component ={About} />
+            <Route path='/projects' component ={Projects} />
+            <Route path='/contact' component={Contact} />
+            <Redirect to="/" />
 
-        </Switch>
+          </Switch>
+
+        </div>
         
         <Footer />
       </Router>
--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import './index.css';
 
 function Contact() {
     return (
-        <div id="contact" className="container">
+        <div id="contact" >
             <h1 className="heading">Contact:</h1>
             <p>Get it touch with me via:</p>
             <ul>
                 <li>Twitter: <a href="https://twitter.com/Fyoung_Jr">@Fyoung_Jr</a></li>
                 <li>Email: <a href="mailto:f.young.de@gmail.com">f.young.de@gmail.com</a></li>
             </ul>
-            <p className="text-center">...and let's build some cool stuffs together!</p>
+            <p>...and let's build some cool stuffs together!</p>
 
         </div>
     )
