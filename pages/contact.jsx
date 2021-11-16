912c244897f8feca552d393bae24e8556b0b62fb
Effiong Bassey <f.young.de@gmail.com>
2021-11-16 18:40:42 +0100

    Added contact details to the contact page

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,7 +1,53 @@
+import { FiMail } from 'react-icons/fi'
+import { FaTwitter, FaLinkedin, FaGithub } from 'react-icons/fa'
+
 import Layout from '../components/Layout'
 
 const Contact = () => {
-  return <Layout>Contact</Layout>
+  return (
+    <Layout>
+      <div className="pl-10 w-max">
+        <div>
+          <a
+            href="mailto:f.young.de@gmail.com"
+            className="flex items-center hover:text-blue-500 transition duration-300"
+          >
+            {' '}
+            <FiMail /> <span className="ml-2">f.young.de@gmail.com</span>
+          </a>
+        </div>
+
+        <div>
+          <a
+            href="https://www.linkedin.com/in/effiong-bassey/"
+            className="flex items-center hover:text-blue-500 transition duration-300 mt-4"
+          >
+            {' '}
+            <FaLinkedin /> <span className="ml-2">Effiong Bassey</span>
+          </a>
+        </div>
+        <div>
+          <a
+            href="https://twitter.com/fyoung_jr"
+            className="flex items-center hover:text-blue-500 transition duration-300 mt-4"
+          >
+            {' '}
+            <FaTwitter /> <span className="ml-2">@fyoung_jr</span>
+          </a>
+        </div>
+
+        <div>
+          <a
+            href="https://github.com/effiong-jr"
+            className="flex items-center hover:text-blue-500 transition duration-300 mt-4"
+          >
+            {' '}
+            <FaGithub /> <span className="ml-2">effiong-jr</span>
+          </a>
+        </div>
+      </div>
+    </Layout>
+  )
 }
 
 export default Contact
