f2a07063231522cb8ae831ee74d7d2250bdddd07
Effiong Bassey <f.young.de@gmail.com>
2021-11-17 09:43:18 +0100

    Modified the about section

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,15 @@ export default function About() {
     <Layout>
       <div className="text-md">
         <p className=" leading-loose">
-          I am an experienced frontend developer with a degree in Social Work. I
-          enjoy engineering my applications to follow a storyline to keep the
-          user engaged and provide a better user experience, using tools like
-          React.js and Redux.js. In my free time, I devote myself to training; I
-          read tech blogs, articles and take many online courses to sustain my
-          growth mindset and to become a better software engineer overall.
+          I am a frontend developer with a passion for building exceptional
+          digital products that delivers on business objectives with modern user
+          interfaces.
+        </p>
+
+        <p className="mt-2">
+          In my free time, I devote myself to training; I read tech blogs,
+          articles and take online courses to sustain my growth mindset and to
+          become a better software engineer overall.
         </p>
 
         <p className="mt-7 text-sm text-gray-300">
@@ -20,7 +23,7 @@ export default function About() {
 
         <div className="mt-10">
           <h3 className="font-semibold text-lg underline mb-1">
-            Tools & Skills
+            Skills & Tools
           </h3>
           <ul className="text-sm pl-7 list-disc">
             <li className="mb-2">React / Next.js</li>
