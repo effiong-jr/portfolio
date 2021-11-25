7eea1e659ce210fcbd8576e9143534a0bba56e5e
Effiong Bassey <f.young.de@gmail.com>
2021-11-26 00:24:46 +0100

    Added google analytics for page views

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,16 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>Portfolio | Effiong Bassey</title>
         <link rel="icon" href="/favicon.ico" />
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA}`}
+        ></script>
+        <script>
+          window.dataLayer = window.dataLayer || []; function gtag()
+          {window.dataLayer.push(arguments)}
+          gtag('js', new Date()); gtag('config', `${process.env.NEXT_PUBLIC_GA}
+          ');
+        </script>
       </Head>
 
       <Component {...pageProps} />
